8c3ca92cba6a5362a72eb43fd27dbd41036e9f5a
Raj Solanki <raj350@mail.fresnostate.edu>
2022-12-28 18:49:44 -0800

    More database updates and frontend for profile page

--- resources/views/business/profile.blade.php
+++ resources/views/business/profile.blade.php
@@ -2,26 +2,52 @@
 
 @section("content")
 <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh; min-width: 100vw;">
-    <form style="width: 40%;" method="POST">
+    <form style="width: 80%;" method="POST">
         @csrf
-        <div class="form-group">
+        <div class="form-group w-100 d-block">
             <h2>Create Profile Page</h2>
         </div>
-        <div class="form-group">
-          <label for="business_type">Add Business Type</label>
-        
-          <select class="custom-select" name="business_type">
-            <option selected>Add Business Type</option>
-            <option value="product">Product</option>
-            <option value="service">Service</option>
-            <option value="equipment">Equipment</option>
-          </select>
 
+        <div class="d-flex flex-row">
+            <div class="left-side w-50">
+                <div class="form-group">
+                    <label for="business_type">Add Business Type</label>
+                  
+                    <select class="custom-select" name="business_type">
+                      <option selected>Add Business Type</option>
+                      <option value="product">Product</option>
+                      <option value="service">Service</option>
+                      <option value="equipment">Equipment</option>
+                    </select>
+                  </div>
+          
+                  <div class="form-group">
+                      <label for="category_1">Category</label>
+                      <div class="d-flex flex-row align-items-baseline">
+                          <select class="custom-select" name="category_1">
+                              <option selected>Add Category Type</option>
+                              <option value="category-item-1">Category Item 1</option>
+                              <option value="category-item-2">Category Item 2</option>
+                              <option value="category-item-3">Category Item 3</option>
+                            </select>
+                            <i class="fa-solid fa-plus" style="cursor: pointer; padding-left: 10px; "></i>
+                      </div>
+                 
+                    </div>
+            </div>
+    
+            <div class="right-side w-50" style="padding-left: 20px;">
+                <div class="form-group">
+                    <label for="business_name">Business Name</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Business Name">
+                    </div>
+                </div>
+    
+            </div>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
+    
+   
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 </div>
